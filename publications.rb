a66e0661aec7451c503271f092115d8140b18b7f
Rafael Henrique Tibães <r@fael.nl>
2014-09-28 17:54:57 -0300

    Authors in IEEE format; Final html version. TODO: sort by date and manual append articles.

--- publications.rb
+++ publications.rb
@@ -3,57 +3,73 @@
 require 'nokogiri'
 require 'open-uri'
 
+# Sample Article: "1. [DOI] [PDF] ANDRADE, B.T., MENDES, C.M., SANTOS JUNIOR, J.O., BELLON, O.R.P. and SILVA, L.: 3D preserving XVIII century barroque masterpiece: challenges and results on the digital preservation of Aleijadinho's sculpture of the Prophet Joel. Journal of Cultural Heritage (2011)."
+
+def to_ieee(authors)
+  authors.collect { |a|
+    names = a.split
+    r = names.pop.gsub(" ", "") + ", "
+    names.each { |n| r << n[0] << "." }
+    r
+  }
+end
+
 class Article
-  attr_accessor :title, :authors, :link, :year, :date
+  attr_accessor :title, :authors, :booktitle, :link, :year, :date
 
   def to_html
-    s = "<b>#{@title}</b>"
-    s << "<i>" << @authors.join(", ") << "</i>"
-    s << "(#{@year})"
+    s = ""
+    s << "<a href=\"#{@link}\">[DOI]</a> " if link
+    s << to_ieee(@authors).join(", ").upcase << ": "
+    s << "<b>#{@title}</b> "
+    s << "<i>#{@booktitle}</i> "
+    s << "(#{@year})."
   end
 end
 
 def parseDblpXml(xmlFile)
   articles = Array.new
-  
+
   doc = Nokogiri::XML(File.open(xmlFile)) { |config|
     config.strict.nonet
   }
-  
-  doc.xpath("//r").each { |article|
+
+  doc.xpath("//r").each { |xmlArticle|
     a = Article.new
-    
-    title = article.search("title").first
+
+    title = xmlArticle.search("title").first
     next unless title
     a.title = title.content if title
-    
-    year = article.search("year").first
+
+    year = xmlArticle.search("year").first
     a.year = year.content if year
-    
-    link = article.search("ee").first
+
+    link = xmlArticle.search("ee").first
     a.link = link.content if link
-    
+
+    booktitle = xmlArticle.search("booktitle").first
+    booktitle = xmlArticle.search("journal").first unless booktitle
+    a.booktitle = booktitle.content if booktitle
+
     a.authors = Array.new
-    article.search("author").each { |author|
+    xmlArticle.search("author").each { |author|
       a.authors << author.content
     }
-    article.search("editor").each { |author|
+    xmlArticle.search("editor").each { |author|
       a.authors << author.content
     }
-    
+
     articles << a
   }
-  
+
   articles
 end
 
 articles = parseDblpXml("Bellon:Olga_Regina_Pereira.xml")
 
-puts articles.first.to_html
-
-
-
-
-
+puts "<h1 style=\"text-align: justify;\">Last Publications</h1>"
+puts "<p style=\"text-align: justify;\">"
+articles.each_index { |aid| puts "#{aid + 1}. #{articles[aid].to_html}" }
+puts "</p>"
 
 
